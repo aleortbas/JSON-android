9bef2f7d8961112e17a27234034b33581bcbc842
Alejandro Ortiz <aleortbas@gmail.com>
2021-10-25 20:16:58 -0500

    Consulta para la tabla canchas

--- get-canchas-by-id.php
+++ get-canchas-by-id.php
@@ -8,20 +8,20 @@ $conn = mysqli_connect($host, $username, $pass)
 or trigger_error (mysqli_error($conn), E_USER_ERROR);
 mysqli_select_db($conn, $db_name);
 
-$local=$_POST['local'];
-//$local="13";
+$idUser=$_POST['id_usuario'];
+//$idUser="80";
 
-
-$query = "select * from numero_canchas where'".$local."' order by id asc";
+$query = "select * from cancha where id_usuario='".$idUser."'";
 //$query = "insert into personas(dni, nombre, telefono, email) values ('1', 'asd', 'asd', 'asd')";
 $query_execute = mysqli_query($conn, $query) or die (mysqli_error($conn));
 
 $arr1 = array();
 while ($row=mysqli_fetch_array($query_execute)) {
-    $arra = array('id' => $row['id'],'local' => $row['local'],'disponible' => $row['disponible']);
+    $arra = array('id' => $row['id'],'nombre' => $row['nombre'],'precio_hora' => $row['precio_hora'],
+    'id_usuario' => $row['id_usuario'],'numero_canchas' => $row['numero_canchas'],'direccion' => $row['direccion']);
     array_push($arr1, $arra);
 	
-    echo json_encode(array('numeroCanchas' => $arr1));
+    echo json_encode(array('canchas' => $arr1));
 	
 
 }
