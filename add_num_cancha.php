45f3578688938481d6116c4ec4e6fbaea0483dc7
Alejandro Ortiz <aleortbas@gmail.com>
2021-10-29 12:45:51 -0500

    add info canchas funciona y tambien update

---
+++ add_num_cancha.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$db_name = "footbocking";
+$username = "root";
+$pass = "";
+$conn = mysqli_connect($host, $username, $pass)
+or trigger_error (mysqli_error($conn), E_USER_ERROR);
+mysqli_select_db($conn, $db_name);
+
+
+$id=$_POST['id'];
+$disponible=$_POST['disponible'];
+$nombre=$_POST['nombre'];
+$idlocal=$_POST['id_local'];
+$imagen=$_POST['imagen'];
+
+$query = "INSERT INTO `numero_canchas`(`id`, `disponible`, `nombre`, `id_Local`, `imagen`) VALUES (NULL,'".$disponible."','".$nombre."','".$idlocal."','".$imagen."')";
+//$query = "INSERT INTO `numero_canchas`(`id`, `disponible`, `nombre`, `id_Local`, `imagen`) VALUES ('[value-1]','[value-2]','value-3','30','[value-5]')";
+$query_execute = mysqli_query($conn, $query) or die (mysqli_error($conn));
+
+
+?>
